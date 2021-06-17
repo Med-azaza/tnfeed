c4b87eb3d2802a02a0b4d679e64ce60ba45cd5b5
med azaza <hamaaz666@gmail.com>
2021-06-17 16:03:33 +0000

    localstorage del

--- app.js
+++ app.js
@@ -1,53 +1,51 @@
 const authup=()=>{
     let submit=document.getElementById('submit');
-submit.addEventListener('click',(e)=>{
-    e.preventDefault();
-    let name=document.getElementById('name').value;
-    let email=document.getElementById('email').value;
-    let pass=document.getElementById('pass').value;
-
-    if(name.length<6){
-        alert('invalid name');
-        return false;
-    }
-    if(email.indexOf('@')<0||email.length<6){
-        alert('invalid email');
-        return false;
-    }
-    if(pass.length<6){
-        alert('password too short');
-        return false;
-    }
-    let userdata={
-        name:name,
-        email:email,
-        password:pass
-    }
-    fetch(`https://tnfeed.herokuapp.com/user/${email}`)
-    .then(res=>res.json())
-    .then((data)=>{
-        if(data==null){
-        fetch('https://tnfeed.herokuapp.com/register',{
-            method:'POST',
-            body:JSON.stringify(userdata),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
+    submit.addEventListener('click',(e)=>{
+        e.preventDefault();
+        let name=document.getElementById('name').value;
+        let email=document.getElementById('email').value;
+        let pass=document.getElementById('pass').value;
+        if(name.length<6){
+            alert('invalid name');
+            return false;
+        }
+        if(email.indexOf('@')<0||email.length<6){
+            alert('invalid email');
+            return false;
+        }
+        if(pass.length<6){
+            alert('password too short');
+            return false;
+        }
+        let userdata={
+            name:name,
+            email:email,
+            password:pass
+        }
+        fetch(`https://tnfeed.herokuapp.com/user/${email}`)
         .then(res=>res.json())
         .then((data)=>{
-            alert('user registred successfully!! login to your account');
+        if(data==null){
+            fetch('https://tnfeed.herokuapp.com/register',{
+                method:'POST',
+                body:JSON.stringify(userdata),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(res=>res.json())
+            .then((data)=>{
+                alert('user registred successfully!! login to your account');
                 fetch('../html/login.html')
-    .then(res=>res.text())
-    .then((data)=>{
-        document.getElementById('root').innerHTML=data;
-        authin();
-})
-    .catch(err=>console.log(err));
-        })
+                .then(res=>res.text())
+                .then((data)=>{
+                    document.getElementById('root').innerHTML=data;
+                    authin();
+                })
+                .catch(err=>console.log(err));
+            })
         }else{
             alert('user already registred');
         }
-        
-    });
+        });
 
 });
 }
@@ -118,7 +116,7 @@ const homepage=(userdata)=>{
     let data={
         owner:userdata.name,
         content: `${content}`,
-        date: `a day in life`
+        date: Date.now()
     }
     document.getElementById('content').value='';
     fetch('https://tnfeed.herokuapp.com/post',{
@@ -140,11 +138,6 @@ refreshFeed();
 
 
 }
-if (localStorage.getItem('data')!=null){
-    homepage(JSON.parse(localStorage.getItem('data')));
-}else{
-
-
 let signup=document.getElementById('signup');
 let login=document.getElementById('login');
 
@@ -166,4 +159,3 @@ login.addEventListener('click',()=>{
 })
     .catch(err=>console.log(err));
 });
-}
